b30fdd80dcdfc7ee3c84698aa755fd6e22f5a436
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-03-24 15:11:04 +0000

    Install flask

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # Execute kill me now process
+
 exec { 'kill_killmenow_process':
-  command => '/usr/bin/pkill killmenow',
-  refreshonly => true,
+    path    => '/bin/',
+    command => 'pkill killmenow',
 }
