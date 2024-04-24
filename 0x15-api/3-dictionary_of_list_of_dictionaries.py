8d82a19c0ec3a0d313358c22422e450cbae97ab2
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-25 00:49:10 +0100

    Export to JSON

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,16 +7,22 @@ import sys
 
 if __name__ == '__main__':
     # Find user by employee id
-    getUsersUrl = (
-        f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
-    )
-    userResponse = requests.get(getUsersUrl)
-    if (userResponse.status_code == 200):
-        user_data = userResponse.json()
-        userName = userResponse.json().get('username')
+    # getUsersUrl = (
+    #     f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
+    # )
+    # userResponse = requests.get(getUsersUrl)
+    # if (userResponse.status_code == 200):
+    #     user_data = userResponse.json()
+    #     userName = userResponse.json().get('username')
 
     # Find todos by userId
     # params = {'userId': sys.argv[1]}
+    getUsersUrl = (
+        f'https://jsonplaceholder.typicode.com/users'
+    )
+    response_users = requests.get(getUsersUrl)
+    users = {user['id']: user['username'] for user in response_users.json()}
+    
     getTodosUrl = (
             f'https://jsonplaceholder.typicode.com/todos'
         )
@@ -28,12 +34,13 @@ if __name__ == '__main__':
         user_id = todo['userId']
         task_title = todo['title']
         completed = todo['completed']
+        username = users.get(user_id, {user_id})
 
         if user_id not in formatted_data:
             formatted_data[user_id] = []
         
         formatted_data[user_id].append({
-            "username": user_id,
+            "username": username,
             "task": task_title,
             "completed": completed
         })
