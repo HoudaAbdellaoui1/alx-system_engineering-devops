922ac9f9d4d58dd25162af0cc23bbfe43b4c0e14
HoudaAbdellaoui1 <abdellaouihouda2@gmail.com>
2024-04-24 11:39:14 +0000

    fix issues

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,11 +5,9 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    employee_id = sys.argv[1]
-
     # Find user by employee id
     getUsersUrl = (
-        f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+        f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
     )
     userResponse = requests.get(getUsersUrl)
     if (userResponse.status_code == 200):
@@ -17,15 +15,14 @@ if __name__ == '__main__':
         userName = userResponse.json().get('name')
 
     # Find todos by userId
-    params = {'userId': employee_id}
+    params = {'userId': sys.argv[1]}
     getTodosUrl = (
             f'https://jsonplaceholder.typicode.com/todos'
         )
     todosResponse = requests.get(getTodosUrl, params=params)
-    if todosResponse.status_code == 200:
-        todos_data = todosResponse.json()
-        completed_counter = sum(1 for t in todos_data if t['completed'])
-        print(f'Employee {userName} is done with tasks ({completed_counter}/{len(todos_data)}):')
-        for t in todos_data:
-            if t['completed']:
-                print(f'\t{t["title"]}')
+    todos_data = todosResponse.json()
+    completed_counter = sum(1 for t in todos_data if t['completed'])
+    print("Employee {} is done with tasks({}/{}):".format(userName, completed_counter, len(todos_data)))
+    for t in todos_data:
+        if t['completed']:
+            print(f'\t{t["title"]}')
