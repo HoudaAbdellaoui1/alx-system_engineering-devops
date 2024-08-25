13faab2e242ecbc2dcb4333f62ed983fc2dd2d02
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-08-25 23:15:39 +0100

    debug 0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,22 +4,22 @@
 import requests
 
 
-
 def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers for a given subreddit.
     If the subreddit is invalid, returns 0.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
-    }
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = (f'https://www.reddit.com/r/{subreddit}/about.json')
     try:
-        subs = requests.get(url, headers=headers, allow_redirects=False)
+        subs = requests.get(url, headers=headers,
+                            allow_redirects=False)
         if subs.status_code == 200:
-            subscribersCount = subs.json().get('data')
-            return subscribersCount.get('subscribers')
+            subscribersCount = subs.json()['data']['subscribers']
+            return subscribersCount
         else:
             return 0
     except requests.RequestException:
+        # Return 0 if there's an error (e.g., network issue, invalid subreddit)
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,9 +9,7 @@ def top_ten(subreddit):
     prints the titles of the first 10 hot posts listed for a given subreddit..
     If not a valid subreddit, print None.
     """
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
-    }
+    headers = {'User-Agent': 'Mozilla/5.0'}
     url = (f'https://www.reddit.com/r/{subreddit}/hot.json')
     params = {'limit': 10}
 
